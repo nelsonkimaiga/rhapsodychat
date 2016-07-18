cbf11eb610324c044fc0b31f3c0cf1ea184c23b9
kimaiga <nelsonkimaiga@aol.com>
2016-07-18 11:55:57 +0300

    added responsive js for mobile view toggle

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+ $(function(){
+
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+
+    $('.nav .toggle-nav').click(function(e){
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+
+});
